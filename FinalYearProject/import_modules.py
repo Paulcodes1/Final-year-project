9f6d2e69facd45f28f44a70150203302a16bb356
Paul Ofili <paulcodes001@gmail.com>
2024-01-31 07:40:29 +0100

    Created a separate file for all my imported libraries.

---
+++ FinalYearProject/import_modules.py
@@ -0,0 +1,8 @@
+import speech_recognition as sr
+import threading
+import tkinter as tk
+from googletrans import Translator
+import time
+from tkinter import filedialog
+from docx import Document
+from reportlab.pdfgen import canvas
--- FinalYearProject/speech2text.py
+++ FinalYearProject/speech2text.py
@@ -1,11 +1,4 @@
-import speech_recognition as sr
-import threading
-import tkinter as tk
-from googletrans import Translator
-import time
-from tkinter import filedialog
-from docx import Document
-from reportlab.pdfgen import canvas
+from import_modules import *
 
 class SpeechTranslatorApp:
     def __init__(self, master):
